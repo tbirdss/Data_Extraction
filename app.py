d8899c843946a8cf877b4be88f69232e7f930d36
tbirdss <58036517+tbirdss@users.noreply.github.com>
2020-07-12 23:12:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
+import jsonify
+import requests
 import re
 import pandas as pd
 import spacy
